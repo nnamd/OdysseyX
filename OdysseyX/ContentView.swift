cdab785dc1d3cb5c3b753559dd50cc421a88bd7c
Nnamdi Nwagwu <nnamdi.nwagwu@gmail.com>
2020-08-29 14:47:53 -0400

    Initial Commit

---
+++ OdysseyX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OdysseyX
+//
+//  Created by Silas Nwagwu on 8/29/20.
+//  Copyright © 2020 Nnamdi Nwagwu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
